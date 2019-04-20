066e637495b4b3def46e17afa60f259e96e0a1f1
KellerAndrew <49701097+KellerAndrew@users.noreply.github.com>
2019-04-20 21:24:20 +0000

    Add files via upload

--- conf.py
+++ conf.py
@@ -39,6 +39,10 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 
+# Index and not contents
+master_doc = 'index'
+
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
